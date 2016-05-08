98d77c5bc137c0cc7758f1a0b51ad051f7dd1843
Maciej Jaskowski <maciej.jaskowski@gmail.com>
2016-05-08 07:17:23 -0400

    Bumping number of epochs.

--- runs/run_dqn43.py
+++ runs/run_dqn43.py
@@ -10,6 +10,7 @@ d['network_type'] = 'nature_with_pad'
 d['updates_method'] = 'deepmind_rmsprop'
 d['weights_dir'] = 'dqn43/weights'
 d['n_test_epochs'] = 0
+d['n_training_epochs'] = 500
 #d['dqn.replay_start_size'] = 100
 
 run.main(**d)
